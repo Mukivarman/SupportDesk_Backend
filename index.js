260fdc100757313f8541fc1edb1f75dcc5b604dd
Mukivarman <mukivarman@gmail.com>
2024-02-07 13:54:56 +0530

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const db=require('./config/db')
 const routes =require('./Router/ProjectRouter')
 const chat=require('./Router/ChatRouter')
-
+const cors=require('cors')
 const app=express();
 db();
 
@@ -11,7 +11,7 @@ app.use(express.json({limit:'5mb'}))
 
 app.use("/api/",routes);
 
-
+app.use(cors());
 
 app.use((req,res,next)=>{
     next();
